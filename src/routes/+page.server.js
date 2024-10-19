55d9808283d84a738ec71938cd9ff1c5906daa6a
Dani Sandoval <dani@equall.ai>
2024-10-19 15:11:38 -0600

    update readme. test functions

---
+++ src/routes/+page.server.js
@@ -0,0 +1,7 @@
+export async function load() {
+    const name = "Dani";
+
+    return {
+        name
+    };
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	export let data;
+
+	const { name } = data;
+</script>
+
+<h1>Hello {name ? name : 'World'}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
