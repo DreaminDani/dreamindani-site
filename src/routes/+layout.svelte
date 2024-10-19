9128253f448a390f819ed30ed6e199fa0e310451
Dani Sandoval <dani@equall.ai>
2024-10-19 16:57:33 -0600

    initial components and basic header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
+	import Header from '$lib/components/layout/header.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
+<Header />
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script>
 	export let data;
+	import { Button } from '$lib/components/ui/button';
 
 	const { name } = data;
 </script>
 
 <h1>Hello {name ? name : 'World'}</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+	Visit <Button variant="link" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</Button> to read
+	the documentation
+</p>
