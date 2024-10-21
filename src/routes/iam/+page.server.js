6dc271543aa8e834f11b186f9541bc8828da3d8a
Dani Sandoval <dani@equall.ai>
2024-10-20 20:30:28 -0600

    iam page

--- src/routes/iam/+page.server.js
+++ src/routes/iam/+page.server.js
@@ -1,16 +1,24 @@
 // Todo remove RSS parser as a dependency
-import { RAINDROP_TEST_TOKEN } from '$env/static/private';
+import { RAINDROP_TEST_TOKEN, LASTFM_API_KEY } from '$env/static/private';
 import { stringify } from 'querystring';
 
 const collectionId = 0;
-const params = {
+const RaindropParams = {
     perpage: 10,
+};
+
+const LastFMParams = {
+    method: 'user.getrecenttracks',
+    user: 'd3sandoval',
+    api_key: LASTFM_API_KEY,
+    format: 'json',
+    limit: 10,
 };
 
 export async function load({ fetch }) {
     let readingList = [];
     try {
-        const response = await fetch(`https://api.raindrop.io/rest/v1/raindrops/${collectionId}?${stringify(params)}`, {
+        const response = await fetch(`https://api.raindrop.io/rest/v1/raindrops/${collectionId}?${stringify(RaindropParams)}`, {
             headers: {
                 Accept: 'application/json',
                 Authorization: `Bearer ${RAINDROP_TEST_TOKEN}`
@@ -18,7 +26,7 @@ export async function load({ fetch }) {
         })
         if (!response.ok) {
             // NOT res.status >= 200 && res.status < 300
-            return { statusCode: response.status, body: response.statusText }
+            throw new Error(`raindrops cannot be retrieved. error: ${response.statusText}`)
         }
         const data = await response.json()
         // console.log(data);
@@ -31,7 +39,28 @@ export async function load({ fetch }) {
         console.log(err) // output to netlify function log
     }
 
+    let nowPlaying = [];
+    try {
+        const response = await fetch(`http://ws.audioscrobbler.com/2.0/?${stringify(LastFMParams)}`, {
+            headers: { Accept: 'application/json' }
+        })
+        if (!response.ok) {
+            // NOT res.status >= 200 && res.status < 300
+            throw new Error(`tracks cannot be retrieved. error: ${response.statusText}`)
+        }
+        const data = await response.json()
+
+        if (data.recenttracks && data.recenttracks.track) {
+            nowPlaying = data.recenttracks.track
+        } else {
+            throw new Error(`tracks cannot be retrieved from ${JSON.toString(data)}`)
+        }
+    } catch (err) {
+        console.log(err) // output to netlify function log
+    }
+
     return {
-        readingList
+        readingList,
+        nowPlaying
     }
 }
--- src/routes/iam/+page.svelte
+++ src/routes/iam/+page.svelte
@@ -1,45 +1,54 @@
 <script>
-	import { fade } from 'svelte/transition';
+	import * as Card from '$lib/components/ui/card';
 
 	export let data;
-	let { readingList } = data;
+	let { readingList, nowPlaying } = data;
 </script>
 
-<div class="content">
-	<div class="listening">
-		<!-- <h1>Listening to...</h1>
-      {#if gettingReadingList}
-        <span>Getting recent tracks</span>
-      {:else}
-        {#each nowPlaying as { artist, album, image, date, url, name, mbid }, index}
-          <a
-            transition:fade={{ delay: index * 250 }}
-            target="_blank"
-            rel="noreferrer nofollow"
-            href={url}
-          >
-            <article class="track" id={mbid}>
-              <img
-                src={image[1]["#text"]}
-                alt={`Album cover for ${artist["#text"]} - ${album["#text"]}`}
-              />
-              <h1>{name}</h1>
-              <h2>{artist["#text"]} - {album["#text"]}</h2>
-              {#if date}
-                <time datetime={date["#text"]}>{date["#text"]}</time>
-              {/if}
-            </article>
-          </a>
-        {/each}
-      {/if} -->
+<div class="flex flex-col m-2 sm:m-4 md:m-0 md:flex-row gap-8">
+	<div class="flex flex-col gap-4 md:basis-1/2">
+		<h1 class="text-4xl font-extrabold mb-4">Listening to...</h1>
+		{#each nowPlaying as { artist, album, image, date, url, name, mbid }, index}
+			<a
+				target="_blank"
+				rel="noreferrer nofollow"
+				href={url}
+				class="hover:cursor-pointer hover:opacity-80 transition-opacity"
+			>
+				<article class="flex gap-4" id={mbid}>
+					<img
+						src={image[1]['#text'] ? image[1]['#text'] : '/placeholder.png'}
+						alt={`Album cover for ${artist['#text']} - ${album['#text']}`}
+						class="h-24 w-24"
+					/>
+					<div class="flex flex-col">
+						<h1 class="font-semibold">{name}</h1>
+						<h2>{artist['#text']} - {album['#text']}</h2>
+						{#if date}
+							<time class="text-muted-foreground" datetime={date['#text']}>{date['#text']}</time>
+						{/if}
+					</div>
+				</article>
+			</a>
+		{/each}
 	</div>
-	<div class="reading">
-		<h1>Currently reading...</h1>
+	<div
+		class="flex flex-col gap-2 md:basis-1/2 focus:shadow-md hover:shadow-md hover:cursor-pointer"
+	>
+		<h1 class="text-4xl font-extrabold mb-4">Currently reading...</h1>
 		{#each readingList as { title, link, excerpt }, index}
-			<article transition:fade={{ delay: index * 250 }}>
-				<a target="_blank" rel="noreferrer nofollow" href={link}>{title}</a>
-				<p class="excerpt">{excerpt}</p>
-			</article>
+			<a target="_blank" rel="noreferrer nofollow" href={link}>
+				<Card.Root class="pb-4">
+					<Card.Header>
+						<Card.Title>
+							{title}
+						</Card.Title>
+						<Card.Description>
+							{excerpt}
+						</Card.Description>
+					</Card.Header>
+				</Card.Root>
+			</a>
 		{/each}
 	</div>
 </div>
