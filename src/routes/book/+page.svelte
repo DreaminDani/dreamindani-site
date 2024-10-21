d3d027f0b0d63c04f0fa53cde94499d56afc189b
Dani Sandoval <dani@equall.ai>
2024-10-20 20:06:26 -0600

    booking page

---
+++ src/routes/book/+page.svelte
@@ -0,0 +1,12 @@
+<div>
+	<script type="text/javascript" async src="https://static.zcal.co/embed/v1/embed.js"></script>
+	<div class="zcal-inline-widget">
+		<a href="https://zcal.co/i/n0KOYf8y">15-30 Minute Meeting - Schedule a meeting</a>
+	</div>
+</div>
+
+<style>
+	#iFrameResizer0 {
+		min-height: 836px !important;
+	}
+</style>
